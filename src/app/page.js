df7bfca366d4453b31137229066def2d2cba3136
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-06-04 14:35:21 +0530

    Little Change

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import '../styles/globals.css';
-import HomePage from '../pages/homePage';
+import HomePage from '../pages/homePage.jsx';
 
 export default function Home() {
   return (
